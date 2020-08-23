62d2e5241a55d258c71d5ffbe6fe4beca3d144ef
Nicole Agurcia <nicole_agurcia@unitec.edu>
2020-08-22 23:10:03 -0600

    default seed for vehicles

--- parking-lot/src/main.ts
+++ parking-lot/src/main.ts
@@ -1,20 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VehicleTypesService } from './vehicle-types/vehicle-types.service';
+import { VehicleService } from './vehicle/vehicle.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const vehicleService = app.get(VehicleTypesService);
-  const types = await vehicleService.getVehicleTypes();
+  const vehicleTypeService = app.get(VehicleTypesService);
+  const vehicleService = app.get(VehicleService);
+  const types = await vehicleTypeService.getVehicleTypes();
+  const vehicles = await vehicleService.getVehicles();
 
   await app.listen(4000);
-
-  if (types.length == 0) {
-    const result = await vehicleService.createVehicleTypes([
+  let vehicleTypesResult;
+  if (!types.length) {
+    vehicleTypesResult = await vehicleTypeService.createVehicleTypes([
       'Residente',
       'Oficial',
     ]);
-    console.log('Created default vehicle types', result);
+    console.log('Created default vehicle types', vehicleTypesResult);
+  }
+
+  if (vehicleTypesResult.length && !vehicles.length) {
+    await vehicleService.createVehicle('RE-2125', vehicleTypesResult[0]._id);
+    await vehicleService.createVehicle('OF-2032', vehicleTypesResult[1]._id);
+    console.log('Created Resident vehicle: RE-2125');
+    console.log('Created Oficial vehicle: OF-2032');
   }
 }
 bootstrap();
